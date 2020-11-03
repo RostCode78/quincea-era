5eb4d88f9e9e4550c6d06b77c1c87939d0631c4b
Jhonatta Tunales <zeroxcode78@gmail.com>
2020-11-03 16:13:20 -0600

    Feat: Item en slider

--- js/showModal.js
+++ js/showModal.js
@@ -4,27 +4,42 @@ let psm = {
    modalContainer: $('.modal-container'),
    btnMenu: $('.btn-line-menu'),
    sobreMiHTML: `
-      <div class="modal-box">
+   <div class="modal-box">
 
-         <div class="modal-sobre-mi-left">
-            <div class="msm-image-box">
-               <div class="msml-image1"></div>
-               <div class="msml-image2"></div>
-               <div class="msml-image3"></div>
+      <div class="modal-sobre-mi-left">
+         <div class="msm-image-box">
+            <div class="container-slider">
+                  <div class="msml-image1"></div>
+                  <div class="msml-image2"></div>
+                  <div class="msml-image3"></div>
             </div>
          </div>
-         <div class="modal-sobre-mi-right">
-            <div class="msmr-top">
-               <div class="cards-img-container">
-                  <div class="card-image1"></div>
-                  <div class="card-image2"></div>
-                  <div class="card-image3"></div>
-               </div>
-            </div>
-            <div class="msmr-bottom"></div>
-         </div>
-
       </div>
+      
+      <div class="modal-sobre-mi-right">
+         <div class="msmr-top">
+            <div class="cards-img-container">
+                  <div class="card-image1 slider-sobre-mi">
+
+                     <div item="1" class="card-opacity co-select1"></div>
+
+                  </div>
+                  <div class="card-image2 slider-sobre-mi">
+
+                     <div item="2" class="card-opacity co-select2"></div>
+
+                  </div>
+                  <div class="card-image3 slider-sobre-mi">
+
+                     <div item="3" class="card-opacity co-select3"></div>
+
+                  </div>
+            </div>
+         </div>
+         <div class="msmr-bottom"></div>
+      </div>
+
+   </div>
    `,
    body: $('body')
 
@@ -47,7 +62,6 @@ let msm = {
       psm.btnMenu.style.zIndex = "9000";
       psm.modalContainer.innerHTML += `${psm.sobreMiHTML}`;
       psm.body.style.overflow = "hidden";
-      þ('HOLA MUNDO');
 
    }
 
